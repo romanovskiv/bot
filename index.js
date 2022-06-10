fe066f97a04974c1c294b513b12241a56858aec3
romanovskiv <89256986+romanovskiv@users.noreply.github.com>
2022-06-10 10:33:30 +0300

    Update index.js
    start func is ready!

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 const { Telegraf, Markup } = require('telegraf');
-const { Keyboard } = require('telegram-keyboard');
+
 const axios = require('axios');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-bot.start((ctx) => {
-    ctx.reply('hi');
-});
-
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.hears('💰 Курсы волют', async(ctx) => {
+bot.hears('💰 Курсы валют', async(ctx) => {
     try {
         const currencyObj = await axios.get(
             'https://www.cbr-xml-daily.ru/daily_json.js'
         );
         return ctx.replyWithMarkdown(
             `
-            📅 **_Курсы на сегодня_** :
+              📅 **_Курсы на сегодня_** :
   
-💵 Доллар:  *${currencyObj.data.Valute.USD.Value}*
-💶 Евро:  *${currencyObj.data.Valute.EUR.Value}*
-🇹🇷 Турецкая лира:  *${currencyObj.data.Valute.TRY.Value}*
-                        
-**_на момент_** — *${currencyObj.data.Date.slice(
-        0,
-        10
-      )}  ${currencyObj.data.Date.slice(11, 25)}*
-`
+  💵 Доллар:  *${currencyObj.data.Valute.USD.Value}*
+  💶 Евро:  *${currencyObj.data.Valute.EUR.Value}*
+  🇹🇷 Турецкая лира:  *${currencyObj.data.Valute.TRY.Value}*
+  
+  **_на момент_** — *${currencyObj.data.Date.slice(
+    0,
+    10
+  )}  ${currencyObj.data.Date.slice(11, 25)}*
+  `
         );
     } catch (error) {
         ctx.reply('Ошибка: ' + error);
     }
     //res.data.Valute.USD
 });
-bot.on('text', async(ctx) => {
-    const keyboard = Keyboard.make([
-        ['💰 Курсы валют'],
-        ['🌤 Прогноз погоды']
-    ]);
-
-    await ctx.reply('Еще не готово', keyboard.reply());
+bot.command('start', (ctx) => {
+    return ctx.reply(
+        'Узнай погоду и курсы валют на сегодня!',
+        Markup.keyboard(['💰 Курсы валют', '🌤 Прогноз погоды'])
+    );
 });
 
 bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
